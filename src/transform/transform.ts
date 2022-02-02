6c07f835dfd9276e8e54ab4d4b6c14aedf8ef4f4
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-02-02 02:18:51 +0100

    fixes

--- src/transform/transform.ts
+++ src/transform/transform.ts
@@ -2,9 +2,8 @@
 /* eslint-disable no-unused-vars */
 
 import { IConfig, IFile, IStyle, Syncify } from 'types';
-import { client } from 'requests/client';
+import { client, queue } from 'requests/client';
 import { Type, isStyle, isMetafield, isSection, asset } from 'config/file';
-import * as metas from 'requests/metafields';
 import { compile as liquid } from 'transform/liquid';
 import { compile as styles } from 'transform/styles';
 import { compile as json } from 'transform/json';
@@ -38,7 +37,7 @@ export function transforms (config: IConfig, cb: typeof Syncify.hook) {
         const style = isStyle(file as IFile<IStyle>, config.transform.styles);
         const data = await readFile(style.config.input);
 
-        await styles(style, data.toString(), request.assets);
+        await styles(style, data.toString(), request.queue.assets);
 
       }
 
@@ -48,18 +47,16 @@ export function transforms (config: IConfig, cb: typeof Syncify.hook) {
 
       else if (is(file.type, Type.Metafield)) {
 
-        if (metas.queue.isPaused) metas.queue.start();
+        if (queue.isPaused) queue.start();
 
         const metafield = isMetafield(file);
         const data = await json(metafield, config.transform.json, cb);
 
-        await request.metafields.queue(
-          {
-            namespace: file.namespace,
-            key: file.key,
-            value: data
-          }
-        );
+        await request.queue.metafields('put', metafield, {
+          namespace: file.namespace,
+          key: file.key,
+          value: data
+        });
 
       }
 
@@ -72,15 +69,7 @@ export function transforms (config: IConfig, cb: typeof Syncify.hook) {
         const section = isSection(file, config.transform.views.sections);
         const attachment = await liquid(section, config.transform.views.minify, cb);
 
-        await request.assets.queue({
-          method: 'put',
-          data: {
-            asset: {
-              key: file.key,
-              attachment
-            }
-          }
-        });
+        await request.queue.assets('put', section, attachment);
 
       }
 
@@ -92,17 +81,7 @@ export function transforms (config: IConfig, cb: typeof Syncify.hook) {
 
         const attachment = await liquid(file, config.transform.views.minify, cb);
 
-        await request.assets.queue(
-          {
-            method: 'put',
-            data: {
-              asset: {
-                key: file.key,
-                attachment
-              }
-            }
-          }
-        );
+        await request.queue.assets('put', file, attachment);
 
       }
 
@@ -114,17 +93,7 @@ export function transforms (config: IConfig, cb: typeof Syncify.hook) {
 
         const attachment = await json(file, config.transform.json, cb);
 
-        await request.assets.queue(
-          {
-            method: 'put',
-            data: {
-              asset: {
-                key: file.key,
-                attachment
-              }
-            }
-          }
-        );
+        await request.queue.assets('put', file, attachment);
 
       }
 
@@ -138,17 +107,7 @@ export function transforms (config: IConfig, cb: typeof Syncify.hook) {
           ? await json(file, config.transform.json, cb)
           : await liquid(file, config.transform.views.minify, cb);
 
-        await request.assets.queue(
-          {
-            method: 'put',
-            data: {
-              asset: {
-                key: file.key,
-                attachment
-              }
-            }
-          }
-        );
+        await request.queue.assets('put', file, attachment);
 
       }
 
@@ -161,17 +120,8 @@ export function transforms (config: IConfig, cb: typeof Syncify.hook) {
         const data = await readFile(file.path);
         const attachment = asset(file, data, cb);
 
-        await request.assets.queue(
-          {
-            method: 'put',
-            data: {
-              asset: {
-                key: file.key,
-                attachment
-              }
-            }
-          }
-        );
+        await request.queue.assets('put', file, attachment);
+
       }
 
     }
@@ -182,14 +132,7 @@ export function transforms (config: IConfig, cb: typeof Syncify.hook) {
 
     else if (is(event, Events.Delete)) {
 
-      await request.assets.queue(
-        {
-          method: 'delete',
-          params: {
-            'asset[key]': file.key
-          }
-        }
-      );
+      await request.queue.assets('delete', file, false);
 
     }
 
