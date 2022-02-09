280aae8b1c95275ce1e54067f35af0e664aa856e
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-02-09 22:19:04 +0100

    exit process when 401

--- src/cli/errors.ts
+++ src/cli/errors.ts
@@ -23,6 +23,7 @@ export function error (file: string, e: {
 
   switch (e.status) {
     case 422:
+
       log.fileError(
         {
           file,
@@ -30,8 +31,10 @@ export function error (file: string, e: {
           data: e.data?.errors?.asset || e.data?.error?.asset
         }
       );
+
       break;
     case 404:
+
       log.fileError(
         {
           file,
@@ -39,8 +42,10 @@ export function error (file: string, e: {
           data: 'The requested resource was not found.'
         }
       );
+
       break;
     case 400:
+
       log.fileError(
         {
           file,
@@ -48,8 +53,10 @@ export function error (file: string, e: {
           data: 'The request was not understood by the server, generally due to bad syntax or because the Content-Type header was not correctly set to application/json. This status is also returned when the request provides an invalid code parameter during the OAuth token exchange process.'
         }
       );
+
       break;
     case 303:
+
       log.fileError(
         {
           file,
@@ -58,16 +65,21 @@ export function error (file: string, e: {
         }
       );
       break;
+
     case 401:
+
       log.fileError(
         {
           file,
           message: e.statusText,
-          data: 'The necessary authentication credentials are not present in the request or are incorrect.'
+          data: 'The necessary authentication credentials are not present in the request or are incorrect.\n'
         }
       );
-      break;
+
+      return process.exit();
+
     case 402:
+
       log.fileError(
         {
           file,
@@ -75,8 +87,10 @@ export function error (file: string, e: {
           data: 'The requested shop is currently frozen. The shop owner needs to log in to the shop\'s admin and pay the outstanding balance to unfreeze the shop.'
         }
       );
+
       break;
     case 406:
+
       log.fileError(
         {
           file,
@@ -84,8 +98,10 @@ export function error (file: string, e: {
           data: 'The requested resource is only capable of generating content not acceptable according to the Accept headers sent in the request.'
         }
       );
+
       break;
     case 423:
+
       log.fileError(
         {
           file,
@@ -93,8 +109,10 @@ export function error (file: string, e: {
           data: 'The requested shop is currently locked. Shops are locked if they repeatedly exceed their API request limit. or if there is an issue with the account, such as a detected compromise or fraud risk.'
         }
       );
+
       break;
     case 403:
+
       log.fileError(
         {
           file,
@@ -102,8 +120,10 @@ export function error (file: string, e: {
           data: 'The server is refusing to respond to the request. This is generally because you have not requested the appropriate scope for this action.'
         }
       );
+
       break;
     case 501:
+
       log.fileError(
         {
           file,
@@ -111,8 +131,10 @@ export function error (file: string, e: {
           data: 'The requested endpoint is not available on that particular shop, e.g. requesting access to a Shopify Plus–only API on a non-Plus shop. This response may also indicate that this endpoint is reserved for future use.'
         }
       );
+
       break;
     case 503:
+
       log.fileError(
         {
           file,
@@ -120,6 +142,7 @@ export function error (file: string, e: {
           data: 'The server is currently unavailable. Check the Shopify status page for reported service outages. See https://www.shopifystatus.com'
         }
       );
+
       break;
     default: log.fileError(
       {
