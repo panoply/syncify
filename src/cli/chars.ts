1d77a27bc89113856154e49c04ea6372ae9bdfa1
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-08-06 02:38:29 +0200

    Create chars.ts

---
+++ src/cli/chars.ts
@@ -0,0 +1,29 @@
+import { line } from './colors';
+
+/**
+ * TUI Tree - Crown
+ *
+ * `┌─`
+ */
+export const crown = line('┌─ ');
+
+/**
+ * TUI Tree - Trunk
+ *
+ * `│`
+ */
+export const trunk = line('│ ');
+
+/**
+ * TUI Tree - Branch
+ *
+ * `├ `
+ */
+export const branch = line('├ ');
+
+/**
+ * TUI Tree - Root
+ *
+ * `└─`
+ */
+export const root = line('└─ ');
