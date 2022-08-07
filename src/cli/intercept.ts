95d31432aaa0266f444e1f110bc6ab6842500411
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-08-07 23:29:23 +0200

    tidy up interceptor

--- src/cli/intercept.ts
+++ src/cli/intercept.ts
@@ -1,57 +1,74 @@
-import { PassThrough } from 'stream';
+import { PassThrough } from 'node:stream';
+import { Console } from 'node:console';
+import { forEach } from 'rambdax';
+
+/* -------------------------------------------- */
+/* TYPES                                        */
+/* -------------------------------------------- */
+
+/**
+ * Callback function method
+ */
+type Callback = (stream: 'stdout' | 'stderr', data: string) => void
 
 /* -------------------------------------------- */
 /* CONSTANTS                                    */
 /* -------------------------------------------- */
 
 /**
- * Intercepts `stdout` and `stderr` console methods
- * and ensure log output is correctly printed. Interception
- * is dynamic and is only enabled during processing to catch
- * any logs from external tooling.
+ * Console Methods
  */
-export function intercept (callback: (stream: 'stdout' | 'stderr', data: string) => void): () => void {
+const methods = [
+  'assert',
+  'count',
+  'countReset',
+  'debug',
+  'dir',
+  'dirxml',
+  'error',
+  'group',
+  'groupCollapsed',
+  'groupEnd',
+  'info',
+  'log',
+  'table',
+  'time',
+  'timeEnd',
+  'timeLog',
+  'trace',
+  'warn'
+];
 
-  /**
-   * Native Methods
-   */
-  const methods = [
-    'assert',
-    'count',
-    'countReset',
-    'debug',
-    'dir',
-    'dirxml',
-    'error',
-    'group',
-    'groupCollapsed',
-    'groupEnd',
-    'info',
-    'log',
-    'table',
-    'time',
-    'timeEnd',
-    'timeLog',
-    'trace',
-    'warn'
-  ];
+/**
+ * Native Methods
+ */
+const native: Map<string, typeof console> = new Map();
 
-  const native: Map<string, unknown> = new Map();
+/**
+ * Console Intercept
+ *
+ * Intercepts `stdout` and `stderr` console methods.
+ * This ensures log output is correctly printed as follows
+ * the TUI aesthetic. Interception is dynamic and is only
+ * enabled during processing to catch any logs from external tooling.
+ */
+export const intercept = (callback: Callback): () => void => {
+
   const stdout: any = new PassThrough();
   const stderr: any = new PassThrough();
 
   stdout.write = (data: string): void => callback('stdout', data); // eslint-disable-line
   stderr.write = (data: string): void => callback('stderr', data); // eslint-disable-line
 
-  const internal = new console.Console(stdout, stderr);
+  const internal = new Console(stdout, stderr);
 
-  for (const method of methods) {
+  forEach((method) => {
     native.set(method, console[method]);
     console[method] = (internal as any)[method];
-  }
+  }, methods);
 
   return () => {
-    for (const method of methods) console[method] = native.get(method);
+    forEach((method) => { console[method] = native.get(method); }, methods);
     native.clear();
   };
 };
