7c434b4b23ade22eb9803540e9bc65e689640318
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-08-28 07:52:57 +0200

    e2e samples

--- test/e2e/custom/src/scripts/components/bar.ts
+++ test/e2e/custom/src/scripts/components/bar.ts
@@ -1,15 +1,13 @@
 import m from 'mithril';
 
-console.log('mous');
-const node = document.createElement('div');
-
-m.mount(node, {
-  view: () => m('h1', 'hello worldssss')
-});
-
-document.body.appendChild(node);
-
 export default function () {
+  console.log('mous');
+  const node = document.querySelector('#some-vnode');
+
+  m.mount(node, {
+    view: () => m('h1', 'hello world')
+  });
+
   console.log('fo');
   return 'basssd';
 }
