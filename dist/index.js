5f10c3ca94e3dbb3f9cd52f5b7940b75ce9237a0
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-08-28 08:00:32 +0200

    add dist to repo

---
+++ dist/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var chunk3O6KLWD6_js = require('./chunk-3O6KLWD6.js');
+
+
+
+Object.defineProperty(exports, 'defineConfig', {
+	enumerable: true,
+	get: function () { return chunk3O6KLWD6_js.b; }
+});
+Object.defineProperty(exports, 'run', {
+	enumerable: true,
+	get: function () { return chunk3O6KLWD6_js.e; }
+});
+Object.defineProperty(exports, 'utils', {
+	enumerable: true,
+	get: function () { return chunk3O6KLWD6_js.d; }
+});
