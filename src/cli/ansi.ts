79eae4c483f709b830533d6a427e88d5df052412
Nikolas Savvidis <nicos.savidis@gmail.com>
2022-08-30 01:02:03 +0200

    add additional ANSIS helpers

--- src/cli/ansi.ts
+++ src/cli/ansi.ts
@@ -1,19 +1,37 @@
 import ansis from 'ansis';
-import { nl } from '../shared/native';
+import { nil, nl } from '../shared/native';
 
 /* -------------------------------------------- */
-/* HELPER UTILITIES                             */
+/* TYPES                                        */
 /* -------------------------------------------- */
 
-/**
- * Clear console but preserve history
- */
-export const clear = '\x1B[H\x1B[2J';
-
-/**
- * Clear console and history
- */
-export const purge = '\x1B[2J\x1B[3J\x1B[H\x1Bc';
+export type Colors = (
+  | 'cyan'
+  | 'cyanBright'
+  | 'red'
+  | 'redBright'
+  | 'green'
+  | 'greenBright'
+  | 'yellow'
+  | 'yellowBright'
+  | 'magenta'
+  | 'magentaBright'
+  | 'blue'
+  | 'blueBright'
+  | 'white'
+  | 'whiteBright'
+  | 'gray'
+  | 'underline'
+  | 'bold'
+  | 'reset'
+  | 'italic'
+  | 'strike'
+  | 'lightGray'
+  | 'neonCyan'
+  | 'neonGreen'
+  | 'orange'
+  | 'pink'
+)
 
 /* -------------------------------------------- */
 /* STANDARD COLORS                              */
@@ -31,7 +49,7 @@ export const {
   magenta,
   magentaBright,
   blue,
-  blackBright,
+  blueBright,
   white,
   whiteBright,
   gray,
@@ -78,16 +96,29 @@ export const neonCyan = ansis.hex('#69d5fd');
 /**
  * TUI Tree - Crown
  *
- * `┌─`
+ * ```
+ * ┌─
+ * ```
  */
 export const open = lightGray('┌─ ');
 
 /**
  * TUI Tree - line
  *
- * `│`
+ * ```
+ * │
+ * ```
  */
 export const line = lightGray('│  ');
+
+/**
+ * TUI Tree - line
+ *
+ * ```
+ * │
+ * ```
+ */
+export const eline = red.dim('│  ');
 
 /**
  * TUI Tree - newline lines, ie: `\n` are prepended and appended
@@ -101,33 +132,75 @@ export const line = lightGray('│  ');
 export const newline = lightGray(`${nl}│${nl}`);
 
 /**
- * TUI Tree - item
- *
- * `├ `
- */
-export const item = lightGray('├  ');
-
-/**
  * TUI Tree - close
  *
- * `└─`
+ * ```
+ * └─
+ * ```
  */
 export const close = lightGray('└─ ');
+
+/* -------------------------------------------- */
+/* HELPER UTILITIES                             */
+/* -------------------------------------------- */
+
+/**
+ * Clear console but preserve history
+ */
+export const clear = '\x1B[H\x1B[2J';
+
+/**
+ * Clear console and history
+ */
+export const purge = '\x1B[2J\x1B[3J\x1B[H\x1Bc';
 
 /* -------------------------------------------- */
 /* EXTENDED HELPERS                             */
 /* -------------------------------------------- */
 
+export const colon = gray(': ');
+
+/**
+ * Arrow character in gray
+ *
+ * ```
+ * →
+ * ```
+ */
+export const arrow = gray(' →  ');
+
 /**
  * Left Parenthesis in gray
  *
- * `(`
+ * ```
+ * (
+ * ```
  */
 export const lpr = gray('(');
 
 /**
  * Right Parenthesis in gray
  *
- * `)`
+ * ```
+ * )
+ * ```
  */
 export const rpr = gray(')');
+
+/**
+ * Warning stdin suffix
+ *
+ * ```
+ * ~ Type w and press enter to view
+ * ```
+ */
+export const warn = yellowBright(` ~ Type ${bold('w')} and press ${bold('enter')} to view`);
+
+/**
+ * Time Suffix
+ *
+ * ```
+ *  ~ 01:59:20
+ * ```
+ */
+export const time = (now: string) => now ? gray(` ~ ${now}`) : nil;
