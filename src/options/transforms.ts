68d3e830200b039df51156e1190b3d03dce60014
Nikolas Savvidis <nicos@gmx.com>
2023-06-29 21:10:38 +0200

    support snippet sub directies
    Thanks to @djang0dev for bringing attention to this

--- src/options/transforms.ts
+++ src/options/transforms.ts
@@ -1,6 +1,6 @@
 import { Config } from 'types';
 import anymatch from 'anymatch';
-import { has } from 'rambdax';
+import { has, isNil } from 'rambdax';
 import { typeError, unknownError, invalidError } from '~log/validate';
 import { bundle, processor } from '~config';
 import * as u from '~utils/native';
@@ -11,14 +11,14 @@ import * as u from '~utils/native';
  * This is a transform option, we will pass to pages
  * in the next validate check,
  */
-export const setViewOptions = (config: Config) => {
+export function setSectionOptions (config: Config) {
 
   if (!has('sections', config.views)) return;
 
   const { sections } = config.views;
 
   // Ensure the section option is an object
-  if (!u.isObject(config.views.sections)) {
+  if (!u.isObject(config.views.sections) && !isNil(config.views.sections)) {
     unknownError('sections', config.views.sections);
   }
 
@@ -35,7 +35,12 @@ export const setViewOptions = (config: Config) => {
         bundle.section[option] = sections[option];
         continue;
       } else {
-        typeError('sections', option, sections[option], 'boolean');
+        typeError({
+          option: 'views.sections',
+          name: option,
+          provided: sections[option],
+          expects: 'boolean'
+        });
       }
     }
 
@@ -49,10 +54,15 @@ export const setViewOptions = (config: Config) => {
           bundle.section[option] = sections[option];
           continue;
         } else {
-          invalidError('sections', option, sections[option], '@ | _ | : | -');
+          invalidError('view.sections', option, sections[option], '@ | _ | : | -');
         }
       } else {
-        typeError('sections', option, sections[option], 'string');
+        typeError({
+          option: 'views.sections',
+          name: option,
+          provided: sections[option],
+          expects: 'string'
+        });
       }
     }
 
@@ -65,7 +75,94 @@ export const setViewOptions = (config: Config) => {
         bundle.section[option] = new RegExp(`${globals.join('|')}`);
         continue;
       } else {
-        typeError('sections', option, sections[option], 'string | string[]');
+        typeError({
+          option: 'views.sections',
+          name: option,
+          provided: sections[option],
+          expects: 'string | string[]'
+        });
+      }
+    }
+  }
+
+};
+
+/**
+ * Section Options
+ *
+ * This is a transform option, we will pass to pages
+ * in the next validate check,
+ */
+export function setSnippetOptions (config: Config) {
+
+  if (!has('snippets', config.views)) return;
+
+  const { snippets } = config.views;
+
+  // Ensure the section option is an object
+  if (!u.isObject(config.views.snippets) && !isNil(config.views.snippets)) {
+    unknownError('snippets', config.views.snippets);
+  }
+
+  // Iterate over all the properties in sections option
+  for (const option in bundle.snippet) {
+
+    // Throw if an undefined property is detected
+    // checks against the default model.
+    if (!has(option, snippets)) unknownError('sections', option);
+
+    // Validate the boolean type values of the option
+    if (option === 'prefixDir') {
+      if (u.isBoolean(snippets[option])) {
+        bundle.snippet[option] = snippets[option];
+        continue;
+      } else {
+        typeError({
+          option: 'views.snippets',
+          name: option,
+          provided: snippets[option],
+          expects: 'boolean'
+        });
+      }
+    }
+
+    // Validate the prefix separator option, in Shopifysnippets
+    // We cannot use dot prefixes, we ensure only accepts values are defined.
+    if (option === 'separator') {
+      if (u.isString(snippets[option])) {
+
+        // Only these character can be prefixers
+        if (/[.@:_-]/.test(snippets[option])) {
+          bundle.section[option] = snippets[option];
+          continue;
+        } else {
+          invalidError('sections', option, snippets[option], '@ | _ | : | - | .');
+        }
+      } else {
+        typeError({
+          option: 'views.snippets',
+          name: option,
+          provided: snippets[option],
+          expects: 'string'
+        });
+      }
+    }
+
+    // Validate the global globs which should have no prefixes applied.
+    if (option === 'global') {
+
+      const globals = u.isString(snippets[option]) ? [ snippets[option] ] : snippets[option];
+
+      if (u.isArray(globals)) {
+        bundle.snippet[option] = new RegExp(`${globals.join('|')}`);
+        continue;
+      } else {
+        typeError({
+          option: 'views.snippets',
+          name: option,
+          provided: snippets[option],
+          expects: 'string | string[]'
+        });
       }
     }
   }
@@ -96,19 +193,34 @@ export function setPageOptions (config: Config) {
           invalidError('pages', option, pages[option], 'markdown | html');
         }
       } else {
-        typeError('pages', option, pages[option], 'string');
+        typeError({
+          option: 'pages',
+          name: option,
+          provided: pages[option],
+          expects: 'string'
+        });
       }
     } else if (option === 'author') {
       if (u.isString(pages[option])) {
         bundle.page[option] = pages[option];
       } else {
-        typeError('pages', option, pages[option], 'string');
+        typeError({
+          option: 'pages',
+          name: option,
+          provided: pages[option],
+          expects: 'string'
+        });
       }
     } else if (option === 'markdown') {
       if (u.isObject(pages[option])) {
         u.assign(bundle.page[option], pages[option]);
       } else {
-        typeError('pages', option, pages[option], 'object');
+        typeError({
+          option: 'pages',
+          name: option,
+          provided: pages[option],
+          expects: 'object'
+        });
       }
     }
   }
@@ -139,7 +251,12 @@ export function setJsonOptions (config: Config) {
         processor.json[option] = json[option];
         continue;
       } else {
-        typeError('json', option, json[option], 'number');
+        typeError({
+          option: 'json',
+          name: option,
+          provided: json[option],
+          expects: 'number'
+        });
       }
     }
 
@@ -149,7 +266,13 @@ export function setJsonOptions (config: Config) {
         processor.json[option] = json[option];
         continue;
       } else {
-        typeError('json', option, json[option], 'boolean');
+        typeError({
+          option: 'json',
+          name: option,
+          provided: json[option],
+          expects: 'boolean'
+        });
+
       }
     }
 
@@ -162,7 +285,12 @@ export function setJsonOptions (config: Config) {
         processor.json[option] = anymatch(exclude as string[]);
         continue;
       } else {
-        typeError('exclude', option, exclude[option], 'string | string[]');
+        typeError({
+          option: 'json',
+          name: option,
+          provided: exclude[option],
+          expects: 'string | string[]'
+        });
       }
     }
 
