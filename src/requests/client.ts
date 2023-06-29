e1a27e84ba77b76f8579bc4b2cab92568b8f10f2
Nikolas Savvidis <nicos@gmx.com>
2023-06-29 21:13:19 +0200

    fuck arrow functions

--- src/requests/client.ts
+++ src/requests/client.ts
@@ -1,9 +1,10 @@
 import { Request, Store, File, Methods, Sync, Requests, Theme } from 'types';
-import { mapParallelAsync } from 'rambdax';
+import { mapAsync, mapParallelAsync } from 'rambdax';
 import { queue } from '../requests/queue';
 import { assign, isUndefined } from '../utils/native';
 import * as asset from '../requests/assets';
 import * as metafields from '../requests/metafields';
+import merge from 'mergerino';
 // import * as pages from '../requests/pages';
 
 /* -------------------------------------------- */
@@ -20,64 +21,71 @@ export type AssetRequest = (method: Methods, file: File, content?: any) => Promi
 
 /* -------------------------------------------- */
 /* CLIENT                                       */
-/* -------------------------------------------- */
 
-export const client = ({ stores, themes }: Sync) => ({
+export function client ({ stores, themes }: Sync) {
 
-  assets: async <T>(method: Methods, file: File<T>, content?: any) => {
+  return ({
 
-    const payload: Partial<Request> = isUndefined(content) ? {
-      method,
-      params: {
-        'asset[key]': file.key
-      }
-    } : {
-      method,
-      data: {
-        asset: {
-          key: file.key,
-          value: content
+    assets: async <T>(method: Methods, file: File<T>, content?: any) => {
+
+      const payload: Partial<Request> = isUndefined(content) ? {
+        method,
+        params: {
+          'asset[key]': file.key
         }
-      }
-    };
+      } : {
+        method,
+        data: {
+          asset: {
+            key: file.key,
+            value: content
+          }
+        }
+      };
 
-    await queue.add(() => mapParallelAsync<Theme, any>(async theme => {
+      await queue.add(() => mapAsync<Theme, any>(async function (theme) {
 
-      await asset.sync(theme, file, assign<any, any, Partial<Request>>(
-        { url: theme.url },
-        stores[theme.sidx].client,
-        payload
-      ));
+        await asset.sync(
+          theme,
+          file,
+          assign<any, any, Partial<Request>>(
+            { url: theme.url },
+            stores[theme.sidx].client,
+            payload
+          )
+        );
 
-    }, themes));
+      }, themes));
 
-  },
-  pages: (content?: Requests.Page) => {
+    },
+    pages: (content?: Requests.Page) => {
 
-    return queue.add(function () {
+      return queue.add(function () {
 
-      return mapParallelAsync<Store, any>(async function (store) {
+        return mapParallelAsync<Store, any>(async function (store) {
 
-        //  await pages.sync(store, content);
+          //  await pages.sync(store, content);
 
-      }, stores);
+        }, stores);
 
-    });
+      });
 
-  },
+    },
 
-  metafields: (content?: Requests.Metafield) => {
+    metafields: (content?: Requests.Metafield) => {
 
-    return queue.add(function () {
+      return queue.add(function () {
 
-      return mapParallelAsync<Store, any>(async function (store) {
+        return mapParallelAsync<Store, any>(async function (store) {
 
-        await metafields.sync(store, content);
+          await metafields.sync(store, content);
 
-      }, stores);
+        }, stores);
 
-    });
+      });
 
-  }
+    }
 
-});
+  });
+
+};
