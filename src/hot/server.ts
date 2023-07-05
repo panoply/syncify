6da50eeb811a883eeb8e5d19401088e49ca66582
Nikolas Savvidis <nicos@gmx.com>
2023-07-05 22:06:41 +0200

    ensure EADDRINUSE fails gracefully when HOT

--- src/hot/server.ts
+++ src/hot/server.ts
@@ -4,12 +4,26 @@ import statics from 'serve-static';
 import handler from 'finalhandler';
 import http from 'node:http';
 import { kill } from '~cli/exit';
-import { log, tui, bold, gray } from '~log';
+import { log, tui, bold, gray, line, redBright, ARR } from '~log';
 import { bundle } from '~config';
 import { injectSnippet, injectRender } from './inject';
 import { pathExists, readFile, writeFile } from 'fs-extra';
-import { isArray } from '~utils/native';
+import { isArray, nl } from '~utils/native';
 import { WSS } from 'types';
+
+export const HOTError: {
+  /**
+   * Whether or not HOT or LIVE Reloading is available
+   */
+  enable: boolean;
+  /**
+   * Output log to be printed.
+   */
+  output: string[];
+} = {
+  enable: true,
+  output: []
+};
 
 async function injection () {
 
@@ -69,6 +83,21 @@ async function injection () {
  */
 export async function server () {
 
+  if (!HOTError.enable) {
+
+    HOTError.output.push(
+      line.red,
+      line.red + redBright('Change the socket port address or kill the session occupying it.'),
+      line.red + redBright('This error typically occurs when multiple Syncify instances are active.')
+    );
+
+    log.err(redBright(`${bold('ERROR')} on ${bold(`${bundle.hot.method === 'hot' ? 'HOT' : 'LIVE'} Reload:`)}`));
+    log.out(HOTError.output.join(nl));
+
+    return null;
+
+  }
+
   log.out(tui.message('whiteBright', bold(`${bundle.hot.method === 'hot' ? 'HOT' : 'LIVE'} Reloading:`)));
   log.nwl();
   log.update(tui.message('gray', 'configuring HOT Reload'));
@@ -86,9 +115,42 @@ export async function server () {
   const server = http.createServer((req, res) => assets(req, res, handler(req, res) as any));
   const localhost = `http://localhost:${bundle.hot.server}`;
 
+  const onerror = (e: { code: 'EADDRINUSE' }) => {
+
+    if (e.code === 'EADDRINUSE') {
+
+      HOTError.output.push(
+        line.red + redBright(`${bold('EADDRINUSE')} ${ARR} ${localhost}`),
+        line.red,
+        line.red + redBright.bold(`Server Port ${bundle.hot.server} address already in use`),
+        line.red,
+        line.red + redBright('Change the server port address or kill the session occupying it.'),
+        line.red + redBright('This error typically occurs when multiple Syncify instances are active.')
+      );
+
+      log.update.clear();
+      log.out(HOTError.output.join(nl));
+      bundle.wss.http.close();
+
+      return null;
+    }
+
+  };
+
+  const onconnect = () => {
+
+    log.update.done();
+
+    server.removeListener('error', onerror);
+    server.removeListener('connect', onconnect);
+
+  };
+
+  server.on('error', onerror);
+  server.on('connect', onconnect);
   server.listen(bundle.hot.server);
 
-  log.out(tui.message('pink', `server → ${bold('assets')} → ${gray.underline(localhost)}`));
+  log.update(tui.message('pink', `server → ${bold('assets')} → ${gray.underline(localhost)}`));
 
 };
 
@@ -107,19 +169,47 @@ export function socket (): WSS {
     path: '/ws'
   });
 
-  kill(() => wss.removeAllListeners());
+  kill(() => wss.close());
 
-  wss.on('connection', v => {
-    wss.on('script', (src) => v.send(`script,${src}`));
-    wss.on('stylesheet', (href) => v.send(`stylesheet,${href}`));
-    wss.on('section', (id) => v.send(`section,${id}`));
-    wss.on('svg', (id) => v.send(`svg,${id}`));
-    wss.on('assets', () => v.send('assets'));
-    wss.on('reload', () => v.send('reload'));
-    wss.on('replace', () => v.send('replace'));
-  });
+  const onerror = (error: { code: 'EADDRINUSE'}) => {
+    if (error.code === 'EADDRINUSE') {
+      wss.close();
+      HOTError.enable = false;
+      HOTError.output.push(
+        line.red + redBright(`${bold('EADDRINUSE')} ${ARR} ws://localhost:${bundle.hot.server}`),
+        line.red,
+        line.red + redBright.bold(`Socket Port ${bundle.hot.server} address already in use`)
+      );
+    }
+  };
+
+  const onclose = () => {
+    wss.removeAllListeners('script');
+    wss.removeAllListeners('stylesheet');
+    wss.removeAllListeners('section');
+    wss.removeAllListeners('svg');
+    wss.removeAllListeners('assets');
+    wss.removeAllListeners('reload');
+    wss.removeAllListeners('replace');
+  };
+
+  const onconnection = (socket: WebSocket) => {
+    wss.removeListener('error', onerror);
+    wss.on('script', (src) => socket.send(`script,${src}`));
+    wss.on('stylesheet', (href) => socket.send(`stylesheet,${href}`));
+    wss.on('section', (id) => socket.send(`section,${id}`));
+    wss.on('svg', (id) => socket.send(`svg,${id}`));
+    wss.on('assets', () => socket.send('assets'));
+    wss.on('reload', () => socket.send('reload'));
+    wss.on('replace', () => socket.send('replace'));
+  };
+
+  wss.on('close', onclose);
+  wss.on('error', onerror);
+  wss.on('connection', onconnection);
 
   return {
+    get http () { return wss; },
     script: (src: string) => wss.emit('script', basename(src)),
     stylesheet: (href: string) => wss.emit('stylesheet', basename(href)),
     section: (id: string) => wss.emit('section', id),
