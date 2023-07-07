0ae0b842e1aefc8d2445ccddb38610c529e5d26a
Nikolas Savvidis <nicos@gmx.com>
2023-07-07 12:18:10 +0200

    v  bump

--- dist/api.js
+++ dist/api.js
@@ -7,10 +7,10 @@ var cjs_js = require('./cjs.js');
 
 
 Object.defineProperty(exports, 'default', {
-  enumerable: true,
-  get: function () { return cjs_js.api_default; }
+	enumerable: true,
+	get: function () { return cjs_js.c; }
 });
 Object.defineProperty(exports, 'defineConfig', {
-  enumerable: true,
-  get: function () { return cjs_js.defineConfig; }
+	enumerable: true,
+	get: function () { return cjs_js.b; }
 });
--- dist/index.js
+++ dist/index.js
@@ -5,14 +5,14 @@ var cjs_js = require('./cjs.js');
 
 
 Object.defineProperty(exports, 'defineConfig', {
-  enumerable: true,
-  get: function () { return cjs_js.defineConfig; }
+	enumerable: true,
+	get: function () { return cjs_js.b; }
 });
 Object.defineProperty(exports, 'env', {
-  enumerable: true,
-  get: function () { return cjs_js.env; }
+	enumerable: true,
+	get: function () { return cjs_js.d; }
 });
 Object.defineProperty(exports, 'run', {
-  enumerable: true,
-  get: function () { return cjs_js.run; }
+	enumerable: true,
+	get: function () { return cjs_js.e; }
 });
