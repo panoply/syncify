12d3ace7705f3608f23af6cf802efe1d2ef57e43
Nikolas Savvidis <nicos@gmx.com>
2023-08-25 10:02:10 +0200

    fixes upload errors

--- lib/modes/upload.ts
+++ lib/modes/upload.ts
@@ -1,141 +1,210 @@
 import type { File, Theme } from 'types';
 import glob from 'fast-glob';
+import { relative } from 'pathe';
 import { readFile } from 'fs-extra';
 import { Syncify } from 'types';
-import { client, queue } from '../requests/client';
-import { outputFile } from '~process/files';
+import { client } from '../requests/client';
+import { Namespace, outputFile } from '~process/files';
 import { error, log, tui } from '~log';
-import { bundle } from '~config';
+import { $ } from '~state';
 import * as n from '~utils/native';
 import * as timer from '~utils/timer';
 import * as c from '~cli/ansi';
-import { has } from 'rambdax';
+import { delay, has } from 'rambdax';
 import { event } from '~utils/utils';
-import { errors } from '~logerrors';
 import { AxiosResponse } from 'axios';
 import { hasSnippet, removeRender } from '~hot/inject';
+import { throwError } from '~options/validate';
+
+interface SyncModel {
+  [store: string]: {
+    [theme: string]: {
+      theme: string;
+      uploaded: number;
+      failed: number;
+      progress: ReturnType<typeof log['progress']>
+      errors: string[]
+    }
+  }
+}
+
+type StoreError = Map<string, Array<{
+  file: File;
+  attempts: number;
+  store: string;
+  theme: string;
+  response: AxiosResponse;
+}>>
+
+interface ErrorModel {
+  local: Array<{
+    file: File;
+    attempts: number;
+    response: string;
+  }>;
+  remote: {
+    [store: string]: StoreError
+  }
+}
+
+function getModel (size: number) {
+
+  if (size === 0) {
+    throwError('Empty output directory', [
+      `There are no files within ${c.blue(relative($.cwd, $.dirs.output))}`,
+      `You may need to run the ${c.blue('syncify build')} command and try again`
+    ]);
+  }
+
+  const errors: ErrorModel = {
+    local: [],
+    remote: {}
+  };
+
+  const sync: SyncModel = {};
+
+  /**
+   * Indentation Width used for CLI logging
+   */
+  let width: number = 0;
+
+  for (const { store, target } of $.sync.themes) {
+
+    if (target.length > width) width = target.length;
+
+    if (!has(store, sync)) sync[store] = {};
+    if (!has(store, errors.remote)) errors.remote[store] = new Map();
+
+    sync[store][target] = {
+      theme: target,
+      uploaded: 0,
+      failed: 0,
+      progress: log.progress(size),
+      errors: []
+    };
+
+  }
+
+  return { sync, errors, size };
+
+}
+
+function hasErrors (sync: SyncModel, errors: ErrorModel) {
+
+  let stores: number = 0;
+
+  for (const store in sync) {
+    if (errors.remote[store].size > 0) {
+      stores = stores + 1;
+    } else {
+      delete errors.remote[store];
+    }
+  }
+
+  return stores;
+}
 
 export async function upload (cb?: Syncify): Promise<void> {
 
-  log.newGroup('Upload');
+  $.cache.lastResource = 'upload';
+
+  log.newGroup('Upload', true);
   log.spinner('Preparing');
 
   timer.start('upload');
 
-  const $: {
-    files: {
-      key: string;
-      namespace: string;
-      fileSize: string;
-      duration: string;
-      elapsed: string;
-    }[],
-    sync: {
-      [store: string]: {
-        theme: string;
-        uploaded: number;
-        failed: number;
-        progress: ReturnType<typeof log['progress']>
-      }[]
-    }
-  } = {
-    files: [],
-    sync: {}
-  };
-
-  const parse = outputFile(bundle.dirs.output);
-  const files = glob.sync(`${bundle.dirs.output}/**`).sort();
-  const request = client(bundle.sync);
+  const request = client($.sync);
   const hashook = n.isFunction(cb);
-  const errs: Map<
-    [store: string, theme: string],
-    Array<[file: File, response: AxiosResponse]>
-  > = new Map();
+  const parse = outputFile($.dirs.output);
+  const files = glob.sync(`${$.dirs.output}/**`).sort();
+  const { sync, errors, size } = getModel(files.length);
 
-  /**
-   * Track successful uploads
-   */
-  let pass = 0;
+  await delay(250);
 
-  /**
-   * Track upload failures
-   */
-  let fail = 0;
+  /* -------------------------------------------- */
+  /* FUNCTIONS                                    */
+  /* -------------------------------------------- */
 
-  let width: number = 0;
-
-  const size = files.length;
-
-  for (const { store, target } of bundle.sync.themes) {
-    if (target.length > width) width = target.length;
-    if (!has(store, $.sync)) $.sync[store] = [];
-    $.sync[store].push({
-      theme: target,
-      uploaded: 0,
-      failed: 0,
-      progress: log.progress(size)
-    });
-  }
-
-  event.on('upload', function (type: 'uploaded' | 'failed', { target, store }: Theme, item) {
+  event.on('upload', function (type: 'uploaded' | 'failed', { target, store }: Theme, item: {
+    key: string;
+    namespace: Namespace;
+    fileSize: string;
+    get file (): File,
+    get error (): AxiosResponse
+  }) {
 
     log.spinner.stop();
 
-    const isErr = type === 'failed';
-
+    const issue = type === 'failed';
     const items = [
       tui.message('whiteBright', c.bold(item.namespace)),
-
       c.newline,
-      tui.message(type === 'failed' ? 'redBright' : 'neonGreen', item.key),
+      tui.message(issue ? 'redBright' : 'neonGreen', item.key),
       c.newline,
-
       tui.suffix('white', 'size ', `  ${item.fileSize}`),
       n.nl,
       tui.suffix('white', 'duration ', `  ${timer.stop()}`),
       n.nl,
       tui.suffix('white', 'elapsed ', `  ${timer.now('upload')}`),
-
       c.newline
-
     ];
 
-    for (const prop in $.sync) {
-      for (const th of $.sync[prop]) {
+    sync[store][target][type] += 1;
+    sync[store][target].progress.increment(1);
 
-        if (prop === store && target === th.theme) {
-          th[type] += 1;
-          th.progress.increment(1);
-        }
+    for (const storeName in sync) {
+      for (const themeName in sync[storeName]) {
 
-        const title = `${c.bold(th.theme.toUpperCase()) + n.wsr(width - target.length)}  ${c.ARR}  ${prop}`;
+        const shop = sync[storeName][themeName];
+        const uploaded = `  ${c.bold(`${shop.uploaded}`)} ${c.white('of')} ${c.bold(`${size}`)}`;
+        const failed = `  ${c.bold(`${shop.failed}`)}`;
 
         items.push(
-          tui.message('neonCyan', title),
+          tui.message('neonCyan', `${c.bold(themeName.toUpperCase())}  ${c.ARR}  ${storeName}`),
           c.newline,
-          tui.suffix('whiteBright', 'uploaded ', `  ${c.bold(`${th.uploaded}`)} ${c.white('of')} ${c.bold(`${size}`)}`),
+          tui.suffix('whiteBright', 'uploaded ', uploaded),
           n.nl,
-          tui.suffix(th.failed > 0 ? 'redBright' : 'white', 'failed ', `  ${c.bold(`${th.failed}`)}`),
+          tui.suffix(shop.failed > 0 ? 'redBright' : 'white', 'failed ', failed),
           c.newline,
-          th.progress.render(false),
+          shop.progress.render(),
           c.newline
         );
 
-        if (isErr) {
-          if (errs.has([ prop, target ])) {
-            errs.get([ prop, target ]).push([ item.file, item.error ]);
-          } else {
-            errs.set([ prop, target ], [ [ item.file, item.error ] ]);
-          }
-        }
-
       }
+    }
 
+    if (issue) {
+
+      sync[store][target].errors.push(item.file.key);
+
+      if (errors.remote[store].has(item.file.output)) {
+
+        errors.remote[store].get(item.file.output).push({
+          store,
+          theme: target,
+          file: item.file,
+          attempts: 0,
+          response: item.error
+        });
+
+      } else {
+        errors.remote[store].set(item.file.output, [
+          {
+            store,
+            theme: target,
+            file: item.file,
+            attempts: 0,
+            response: item.error
+          }
+        ]);
+      }
     }
 
     log.update(n.glue(items));
 
   });
+
+  await delay(500);
 
   for (const path of files) {
 
@@ -145,7 +214,7 @@ export async function upload (cb?: Syncify): Promise<void> {
 
     try {
 
-      const read = await readFile(file.input);
+      const read = await readFile(file.output);
 
       input = read.toString();
 
@@ -175,68 +244,71 @@ export async function upload (cb?: Syncify): Promise<void> {
         }
       }
 
-      pass++;
-
     } catch (e) {
 
-      console.log(e);
-      // errs.push([ file, e ]);
-
-      fail++;
-
-      // update({
-      //   key: file.key,
-      //   color: 'redBright',
-      //   namespace: file.namespace,
-      //   fileSize: getSizeStr(input),
-      //   duration: timer.stop(),
-      //   elapsed: timer.now()
-      // });
+      errors.local.push({
+        file,
+        attempts: 0,
+        response: e.message
+      });
 
     }
 
   }
 
-  // log.spinner.stop();
+  log.update.clear();
 
-  // log.update(
-  //   n.glue([
-  //     tui.message('whiteBright', c.bold('Completed')),
-  //     c.newline,
-  //     tui.suffix('neonGreen', 'uploaded ', `  ${c.bold(`${pass}`)} ${c.white('of')} ${c.bold(`${size}`)}`),
-  //     n.nl,
-  //     tui.suffix(fail > 0 ? 'redBright' : 'white', 'errors ', `  ${c.bold(`${fail}`)}`),
-  //     n.nl,
-  //     tui.suffix('white', 'duration ', `  ${timer.stop()}`)
-  //   ])
-  // );
+  for (const storeName in sync) {
+    for (const themeName in sync[storeName]) {
 
-  // log.update.done();
-  // log.nwl();
+      const shop = sync[storeName][themeName];
 
-  // if (fail > 0) {
+      if (shop.failed === 0) continue;
 
-  //   log.write([
-  //     c.gray(`The below ${c.bold(`${fail}`)} files failed to sync to and were not uploaded.`),
-  //     c.gray(`Type ${c.blue.bold('v')} and press ${c.blue.bold('enter')} to view error responses from Shopify.`)
-  //   ]);
+      const name = c.bold(`${themeName.toUpperCase()} THEME`);
+      const failed = `  ${c.bold(`${shop.failed}`)}`;
+      const uploaded = `  ${c.bold(`${shop.uploaded}`)} ${c.white('of')} ${c.bold(`${size}`)}`;
 
-  //   log.nwl();
+      log.out(tui.message('whiteBright', `${c.bold(name)}  ${c.ARR}  ${storeName}`));
+      log.nwl();
+      log.out(tui.suffix('whiteBright', 'uploaded ', uploaded));
+      log.out(tui.suffix('whiteBright', 'failed ', failed));
+      log.nwl();
+      log.write([
+        c.gray('Syncify is now watching all rejected files.'),
+        c.gray('Changes will be resynced if successful.')
+      ]);
 
-  //   for (const [ file, e ] of errs) {
-  //     if (!has(file.key, errors)) errors[file.key] = new Set();
-  //     errors[file.key].add(error.request<string>(file.input, e, false));
-  //     log.write(`${c.DSH} ${c.redBright(file.key)}`);
-  //   }
+      let number: number = 1;
+      for (const [ file, item ] of errors.remote[storeName].entries()) {
 
-  //   log.nwl();
+        const theme = item.find(({ theme }) => theme === themeName);
 
-  // } else {
+        if (theme) {
+          const errno = `${(number < 10 ? '0' : '') + number++}`;
+          log.nwl();
+          log.write(c.redBright.bold(`ERROR ${errno}`));
+          error.request(file, theme.response);
+        }
+      }
 
-  //   return queue.onIdle().then(() => {
-  //     tui.closer('Upload');
-  //     process.exit(0);
-  //   });
+      log.out(c.hr(20));
 
-  // }
+    }
+  }
+
+  const issues = hasErrors(sync, errors);
+
+  if (issues > 0) {
+
+    log.write([
+      c.white('Syncify is now watching file for changes.'),
+      c.white(`The file failed to sync with ${2} on. Below are the errors:`)
+    ]);
+
+    log.nwl();
+    log.write(c.magenta('Waiting for changes...'));
+
+  }
+
 };
