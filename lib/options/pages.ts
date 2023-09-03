36cd6417742816fb6e910df13b5321c8ebdc79de
Nikolas Savvidis <nicos@gmx.com>
2023-09-03 20:38:19 +0200

    support page sync

--- lib/options/pages.ts
+++ lib/options/pages.ts
@@ -6,7 +6,7 @@ import * as u from '~utils/native';
 
 export function setPageOptions (config: Config) {
 
-  if (has('pages', config.views)) return;
+  if (!has('pages', config.views)) return;
 
   const { pages } = config.views;
 
@@ -23,53 +23,89 @@ export function setPageOptions (config: Config) {
   // Skip if empty object
   if (isEmpty(pages)) return;
 
+  const defaults = $.config.views.pages;
+
   // Iterate over all the properties in sections option
   for (const option in pages) {
 
     // Throw if an undefined property is detected
     // checks against the default model.
-    if (!has(option, $.page)) unknownError('pages', option);
+    if (!has(option, defaults)) unknownError('pages', option);
 
-    if (option === 'language') {
+    if (option === 'importLanguage') {
+
       if (u.isString(pages[option])) {
         if (pages[option] === 'markdown' || pages[option] === 'html') {
           $.page[option] = pages[option];
         } else {
-          invalidError('pages', option, pages[option], 'markdown | html');
+          invalidError('views.pages', option, pages[option], 'markdown | html');
         }
       } else {
         typeError({
-          option: 'pages',
+          option: 'views.pages',
           name: option,
           provided: pages[option],
-          expects: 'string'
+          expects: 'markdown | html'
         });
       }
+
     } else if (option === 'author') {
+
+      if (isNil(pages[option])) continue;
+
       if (u.isString(pages[option])) {
+
         $.page[option] = pages[option];
+
       } else {
         typeError({
-          option: 'pages',
+          option: 'views.pages',
           name: option,
           provided: pages[option],
           expects: 'string'
         });
       }
-    } else if (option === 'markdown') {
 
-      if (u.isObject(pages[option])) {
+    } else if (option === 'suffixDir' || option === 'safeSync') {
 
-        u.assign($.page[option], pages[option]);
+      if (isNil(pages[option])) continue;
+
+      if (u.isBoolean(pages[option])) {
+
+        $.page[option] = pages[option];
 
       } else {
+
         typeError({
-          option: 'pages',
+          option: 'views.pages',
           name: option,
           provided: pages[option],
           expects: 'object'
         });
       }
+
+    } else if (option === 'global') {
+
+      if (isNil(pages[option])) continue;
+
+      if (u.isArray(pages[option])) {
+
+        if (pages[option].length > 0) {
+          $.page[option] = new RegExp(`${pages[option].join('|')}`);
+          continue;
+        }
+
+      } else {
+
+        typeError({
+          option: 'views.pages',
+          name: option,
+          provided: pages[option],
+          expects: 'string | string[]'
+        });
+
+      }
+
     }
   }
 
