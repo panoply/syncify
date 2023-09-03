0d66693b27e001c8b04eeac3ea7a9d22e8b9bada
Nikolas Savvidis <nicos@gmx.com>
2023-09-03 20:36:16 +0200

    Update files.ts

--- lib/process/files.ts
+++ lib/process/files.ts
@@ -1,12 +1,12 @@
 /* eslint-disable no-unused-vars */
 
-import { join, parse, relative, extname, basename } from 'node:path';
-import { File, Paths, PathsBundle } from 'types';
-import { assign, nil } from '~utils/native';
+import { join, parse, relative, basename } from 'pathe';
+import { File, PathBundle } from 'types';
+import { assign } from '~utils/native';
 import { lastPath } from '~utils/paths';
 import { Partial } from 'rambdax';
 import * as context from '~process/context';
-import { bundle } from '~config';
+import { $ } from '~state';
 import merge from 'mergerino';
 import { Tester } from 'anymatch';
 import { uuid } from '~utils/utils';
@@ -23,6 +23,7 @@ export const enum Type {
   Layout,
   Snippet,
   Section,
+  Metaobject,
   Config,
   Locale,
   Style,
@@ -46,6 +47,7 @@ export const enum Namespace {
   Layout = 'layout',
   Templates = 'templates',
   Customers = 'templates/customers',
+  Metaobject = 'templates/metaobject',
   Config = 'config',
   Locales = 'locales',
   Assets = 'assets',
@@ -54,9 +56,8 @@ export const enum Namespace {
 }
 
 /**
- * File kinds represent the processing types,
- * typicaly infers the tranform operation to
- * be applied.
+ * File kinds represent the processing types, typicaly infers the transform
+ * operation to be applied.
  */
 export const enum Kind {
   JSON = 'JSON',
@@ -124,7 +125,7 @@ export function setFile (parsedFile: Partial<File>, input: string, output: strin
 
   const file = <File>parsedFile;
 
-  return <T extends unknown>(namespace: string, type: Type, kind?: Kind): File<T> => {
+  return <T extends unknown>(namespace: string, type: Type, kind?: Kind | -1): File<T> => {
 
     let key: string;
 
@@ -136,6 +137,8 @@ export function setFile (parsedFile: Partial<File>, input: string, output: strin
       output = join(output, key);
     }
 
+    if (kind === -1) input = $.cache.maps[input];
+
     return assign({}, file, {
       uuid: uuid(),
       type,
@@ -144,7 +147,7 @@ export function setFile (parsedFile: Partial<File>, input: string, output: strin
       key,
       namespace,
       kind,
-      relative: relative(bundle.cwd, input),
+      relative: relative($.cwd, input),
       size: NaN,
       data: undefined
     });
@@ -162,7 +165,7 @@ export function setFile (parsedFile: Partial<File>, input: string, output: strin
  * @param paths The Anymatch tester
  * @param output The output base directory path
  */
-export function parseFile (paths: PathsBundle, output: string) {
+export function parseFile (paths: PathBundle, output: string) {
 
   return (path: string) => {
 
@@ -171,40 +174,70 @@ export function parseFile (paths: PathsBundle, output: string) {
 
     if (file.ext === '.liquid') {
 
-      if (paths.sections(path)) {
+      if (paths.sections.match(path)) {
         return context.section(define('sections', Type.Section, Kind.Liquid));
-      } else if (paths.snippets(path)) {
+      } else if (paths.snippets.match(path)) {
         return context.snippet(define('snippets', Type.Snippet, Kind.Liquid));
-      } else if (paths.layout(path)) {
+      } else if (paths.layout.match(path)) {
         return define('layout', Type.Layout, Kind.Liquid);
-      } else if (paths.templates(path)) {
+      } else if (paths.templates.match(path)) {
         return define('templates', Type.Template, Kind.Liquid);
-      } else if (paths.customers(path)) {
+      } else if (paths.customers.match(path)) {
         return define('templates/customers', Type.Template, Kind.Liquid);
+      } else if (paths.metaobject.match(path)) {
+        return define('templates/metaobject', Type.Template, Kind.Liquid);
       } else if (paths.transforms.has(path)) {
-        switch (paths.transforms.get(path)) {
-          case Type.Style: return context.style(define('snippets', Type.Style, Kind.CSS));
+        if (paths.transforms.get(path) === Type.Style) {
+          return context.style(define('snippets', Type.Style, Kind.CSS));
         }
+      }
+
+    } else if (paths.assets.match(path)) {
+
+      if ($.spawn.invoked) {
+        return define('assets', Type.Spawn);
+      } else if (file.ext === '.jpg' || file.ext === '.png' || file.ext === '.gif' || file.ext === '.pjpg') {
+        return define('assets', Type.Asset, Kind.Image);
+      } else if (file.ext === '.mov' || file.ext === '.mp4' || file.ext === '.webm' || file.ext === '.ogg') {
+        return define('assets', Type.Asset, Kind.Video);
+      } else if (file.ext === '.pdf') {
+        return define('assets', Type.Asset, Kind.PDF);
+      } else if (file.ext === '.eot' || file.ext === '.ttf' || file.ext === '.woff' || file.ext === '.woff2') {
+        return define('assets', Type.Asset, Kind.Font);
+      } else if (file.ext === '.js' || file.ext === '.mjs') {
+        return define('assets', Type.Asset, Kind.JavaScript);
+      } else if (file.ext === '.json') {
+        return define('assets', Type.Asset, Kind.JSON);
+      } else if (file.ext === '.svg') {
+        return define('assets', Type.Asset, Kind.SVG);
+      } else if (file.ext === '.css') {
+        return define('assets', Type.Asset, Kind.CSS);
       }
 
     } else if (file.ext === '.md' || file.ext === '.html') {
 
-      return define('pages', Type.Page, file.ext === '.html' ? Kind.HTML : Kind.Markdown);
+      if (file.ext === '.html') {
+        return define('pages', Type.Page, Kind.HTML);
+      } else {
+        return define('pages', Type.Page, Kind.Markdown);
+      }
 
     } else if (file.ext === '.json') {
 
-      if (paths.metafields(path)) {
+      if (paths.metafields.match(path)) {
         return define('metafields', Type.Metafield, Kind.JSON);
-      } else if (paths.sections(path)) {
+      } else if (paths.sections.match(path)) {
         return context.section(define('sections', Type.Section, Kind.JSON));
-      } else if (paths.templates(path)) {
+      } else if (paths.templates.match(path)) {
         return define('templates', Type.Template, Kind.JSON);
-      } else if (paths.config(path)) {
+      } else if (paths.config.match(path)) {
         return define('config', Type.Config, Kind.JSON);
-      } else if (paths.locales(path)) {
+      } else if (paths.locales.match(path)) {
         return define('locales', Type.Locale, Kind.JSON);
-      } else if (paths.customers(path)) {
+      } else if (paths.customers.match(path)) {
         return define('templates/customers', Type.Template, Kind.JSON);
+      } else if (paths.metaobject.match(path)) {
+        return define('templates/metaobject', Type.Template, Kind.JSON);
       }
 
     } else if (file.ext === '.svg') {
@@ -225,22 +258,10 @@ export function parseFile (paths: PathsBundle, output: string) {
     } else if (file.ext === '.tsx') {
       return context.script(define('assets', Type.Script, Kind.TSX));
 
-    } else if (paths.assets(path)) {
-
-      if (bundle.spawn.invoked) {
-        return define('assets', Type.Spawn);
-      } else if (file.ext === '.jpg' || file.ext === '.png' || file.ext === '.gif' || file.ext === '.pjpg') {
-        return define('assets', Type.Asset, Kind.Image);
-      } else if (file.ext === '.mov' || file.ext === '.mp4' || file.ext === '.webm' || file.ext === '.ogg') {
-        return define('assets', Type.Asset, Kind.Video);
-      } else if (file.ext === '.pdf') {
-        return define('assets', Type.Asset, Kind.PDF);
-      } else if (file.ext === '.eot' || file.ext === '.ttf' || file.ext === '.woff' || file.ext === '.woff2') {
-        return define('assets', Type.Asset, Kind.Font);
-      }
     }
 
     return undefined;
+
   };
 };
 
@@ -263,10 +284,12 @@ export function importFile (output: string) {
       return merge('snippets', Type.Snippet);
     } else if (path.startsWith('layout/')) {
       return merge('layout', Type.Layout);
-    } else if (path.startsWith('templates/')) {
-      return merge('templates', Type.Template);
     } else if (path.startsWith('customers/', 10)) {
       return merge('templates/customers', Type.Template);
+    } else if (path.startsWith('metaobject/', 10)) {
+      return merge('templates/metaobject', Type.Template);
+    } else if (path.startsWith('templates/')) {
+      return merge('templates', Type.Template);
     } else if (path.startsWith('config/')) {
       return merge('config', Type.Config);
     } else if (path.startsWith('locales/')) {
@@ -285,14 +308,15 @@ export const outputFile = (output: string) => (path: string) => {
   const merge = setFile(file, path, output);
 
   switch (basename(file.dir)) {
-    case 'sections': return merge('sections', Type.Section);
-    case 'snippets': return merge('snippets', Type.Snippet);
+    case 'sections': return merge('sections', Type.Section, -1);
+    case 'snippets': return merge('snippets', Type.Snippet, -1);
     case 'layout': return merge('layout', Type.Layout);
-    case 'templates': return merge('templates', Type.Template);
-    case 'customers': return merge('templates/customers', Type.Template);
-    case 'config': return merge('config', Type.Config);
-    case 'locales': return merge('locales', Type.Locale);
-    case 'assets': return merge('assets', Type.Asset);
+    case 'templates': return merge('templates', Type.Template, -1);
+    case 'customers': return merge('templates/customers', Type.Template, -1);
+    case 'metaobject': return merge('templates/metaobject', Type.Template, -1);
+    case 'config': return merge('config', Type.Config, -1);
+    case 'locales': return merge('locales', Type.Locale, -1);
+    case 'assets': return merge('assets', Type.Asset, -1);
   }
 
 };
