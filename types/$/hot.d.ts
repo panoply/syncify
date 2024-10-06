a35df2630c3e7f1873e21246876e705365f9f916
Nikolas Savvidis <nicos@gmx.com>
2024-10-06 04:45:13 +0200

    overhaul HOT options

--- types/$/hot.d.ts
+++ types/$/hot.d.ts
@@ -1,10 +1,8 @@
 /* -------------------------------------------- */
 /* LIVE RELOADS                                 */
 /* -------------------------------------------- */
-
-// import type { Server, WebSocket } from 'ws';
-// import type { IncomingMessage } from 'http';
-import { TemplatedApp } from 'uWebSockets.js';
+import type { TemplatedApp } from 'uWebSockets.js';
+import type { HOT } from '../config/hot';
 
 export interface WSS {
   /**
@@ -58,119 +56,14 @@ export interface WSS {
 /**
  * **INTERNAL USE**
  */
-export interface HOTBundle {
+export interface HOTBundle extends HOT {
   /**
-   * Whether or not Syncify hot reloads UI labels should render.
-   *
-   * @default true
-   */
-  label?: 'visible' | 'hidden';
-  /**
-   * **NOT YET AVAILABLE**
-   *
-   * Whether or not to record changes. When enabled, Syncify
-   * will keep track of the last 25 changes applied and allow
-   * you to walk through the stack.
-   */
-  history?: boolean;
-  /**
-   * Whether or not Syncify should inject the required HOT snippet
-   * at runtime layout/s. When `false` you will need to manually place
-   * the `hot.js.liquid` snippet into your theme.
-   *
-   * _Please note that by default when running `--hot` Syncify will
-   * check your layout/s for the hot snippet and if it's not present then
-   * syncify will inject it and invoke an upload of the layouts._
-   *
-   * @default true
-   */
-  inject?: boolean;
-  /**
-   * A string list of Liquid template layout names used in your theme
-   * which should have the hot snippet injected.
-   *
-   * @default ['theme.liquid']
-   */
-  layouts?: string[];
-  /**
-   * The static server for assets - This will be written in the HOT snippet
-   *
-   * @default 3000
-   * @example 'http://localhost:3000/some-asset.js'
-   */
-  server?: number;
-  /**
-   * Websocket port - - This will be written in the HOT snippet
-   *
-   * @default 8089
-   * @example 'ws://localhost:8089/ws'
-   */
-  socket?: number;
-  /**
-   * Which live reload method should Syncify use?
-   *
-   * **hot**
-   *
-   * _Hot reloads assets and views with automatic refresh upon changes_
-   *
-   * **refresh**
-   *
-   * _Invokes a full page refresh after changes have been applied_
-   *
-   *
-   * @default 'hot'
-   */
-  method?: 'hot' | 'refresh';
-  /**
-   * The HOT strategy to use. Syncify supports 2 different
-   * replacement strategies and you will need choose which one
-   * to use depending on your project type.
-   *
-   *
-   * **hydrate**
-   *
-   * _The hydrate strategy will execute morph replacements. This is what Syncify
-   * will default to, however it is not always perfect and in cases where you leverage
-   * frameworks that use DOM Mutation observers, it probably better to use `replace`_
-   *
-   * **replace**
-   *
-   * _The replace strategy will execute fragment swaps use `replaceWith` instead of morphs
-   * when executing HOT reloads. It works almost identical to `hydrate` but respects DOM
-   * mutations. If you are leveraging a framework like Stimulus or Alpine, then choose this
-   * strategy._
-   *
-   *
-   * @default 'hydrate'
-   */
-  strategy?: 'hydrate' | 'replace';
-  /**
-   * Scroll position between reloads
-   *
-   * @default 'preserved'
-   */
-  scroll?: 'preserved' | 'top';
-  /**
-   * The liquid render tag written to Layouts.
+   * The resolved location of the hot injection source
    *
    * @default
-   * {% render 'hot.js.liquid', server: 3000, socket: 8089 %}
+   * 'node_modules/@syncify/cli/hot.min.js'
    */
-  renderer?: string;
-  /**
-   * The hot reload inline snippet script URI location
-   *
-   * @default
-   * 'node_modules/@syncify/cli/hot.js.liquid'
-   */
-  snippet?: string;
-  /**
-   * The resolved output uri the snippet will be written.
-   *
-   * @default
-   * 'theme/snippets/hot.js.liquid'
-   */
-  output?: string;
+  source?: string;
   /**
    * Which templates contain the hot snippet. The `key` value
    * will hold the URI output path where to file.
@@ -179,14 +72,3 @@ export interface HOTBundle {
    */
   alive?: { [template: string]: boolean; };
 }
-
-export type HOTConfig = Pick<HOTBundle,
-  | 'inject'
-  | 'label'
-  | 'layouts'
-  | 'method'
-  | 'strategy'
-  | 'server'
-  | 'socket'
-  | 'scroll'
->
