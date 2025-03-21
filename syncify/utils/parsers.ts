3464e909c9ec410bda30e0e4d4ca3dffbc493ed7
Nikolas Savvidis <nicos@gmx.com>
2025-03-21 06:17:01 +0100

    Update parsers.ts

--- syncify/utils/parsers.ts
+++ syncify/utils/parsers.ts
@@ -8,7 +8,7 @@ import { parse } from '@syncify/json';
  */
 export async function parseToml <T> (uri: string): Promise<T> {
 
-  const toml = await $import<'toml'>('smol-toml');
+  const toml = await $import('smol-toml');
   const file = await readFile(uri, 'utf-8');
 
   return <T> toml.parse(file);
@@ -20,7 +20,7 @@ export async function parseToml <T> (uri: string): Promise<T> {
  */
 export async function writeToml <T> (uri: string, input: T) {
 
-  const toml = await $import<'toml'>('smol-toml');
+  const toml = await $import('smol-toml');
   const file = toml.stringify(input);
 
   return writeFile(uri, file);
@@ -44,7 +44,7 @@ export async function parseJson (uri: string) {
 export async function parseYaml <T> (uri: string): Promise<T> {
 
   const file = await readFile(uri, 'utf-8');
-  const yaml = await $import<'yaml'>('js-yaml');
+  const yaml = await $import('js-yaml');
 
   return <T>yaml.load(file);
 
@@ -52,7 +52,7 @@ export async function parseYaml <T> (uri: string): Promise<T> {
 
 export async function writeYaml <T> (uri: string, input: T) {
 
-  const yaml = await $import<'yaml'>('js-yaml');
+  const yaml = await $import('js-yaml');
   const file = yaml.dump(input);
 
   return writeFile(uri, file);
