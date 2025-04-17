190152cfa0f3508156e89019ea5d451559c5cf17
Nikolas Savvidis <nicos@gmx.com>
2025-04-17 06:16:07 +0200

    fixes themeList selection

--- syncify/http/themeFiles/themesList.ts
+++ syncify/http/themeFiles/themesList.ts
@@ -9,21 +9,23 @@ export function themesList (store: Type.Store) {
   return new Promise<Type.OnlineStoreTheme[]>((resolve, reject) => {
 
     http(store.name).request<Type.QueryThemes>({
-      data: gql`
-        query ThemeList {
-          themes(first: 100) {
-            nodes {
-              id
-              createdAt
-              name
-              prefix
-              role
-              themeStoreId
-              updatedAt
+      data: {
+        query: gql`
+          query ThemeList {
+            themes(first: 100) {
+              nodes {
+                id
+                createdAt
+                name
+                prefix
+                role
+                themeStoreId
+                updatedAt
+              }
             }
           }
-        }
-      `
+        `
+      }
     }).then(({
       data: {
         themes: {
