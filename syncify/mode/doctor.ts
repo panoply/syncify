a9f38967d572353ac87d88f7eea3de8ba101e21d
Nikolas Savvidis <nicos@gmx.com>
2025-04-26 16:20:53 +0200

    doctor doctor

--- syncify/mode/doctor.ts
+++ syncify/mode/doctor.ts
@@ -1,4 +1,18 @@
-export function doctor () {
+import * as _ from '@syncify/ansi';
+
+export function Doctor () {
+
+  const write = _.Create()
+  .Header('Syncify Doctor 🩺', _.bold)
+  .Wrap(_.gray, 'Doctor mode will attempt to diagnose and treat configuration issues.')
+  .Template({ id: 'version', prefix: true })
+  .Template({ id: 'caches', prefix: true })
+  .Template({ id: 'credentials', prefix: true })
+  .Template({ id: 'projects', prefix: true })
+  .Template({ id: 'installation', prefix: true })
+  .Template({ id: 'location', prefix: true })
+  .Template({ id: 'structure', prefix: true })
+  .toLog();
 
   // STEP 1 - Look for cache home store and generate is non existent
   // STEP 2 - Look for cache if none exists generate them
