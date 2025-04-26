5b8b49f3d12806d522f1ffa4d5e82e50c1e9e7b9
Nikolas Savvidis <nicos@gmx.com>
2025-04-26 16:08:43 +0200

    Revert "fixes package.json config identification"
    This reverts commit 9776516911048c07e48e97f3e3ba80b318ec3b07.

--- syncify/options/define/caches.ts
+++ syncify/options/define/caches.ts
@@ -4,7 +4,7 @@ import { ensureDir, ensureDirSync, mkdirSync, pathExists, pathExistsSync } from 
 
 import { updateProject } from './project';
 
-import { throws } from '~cli/throws';
+import { unknownProject } from '~cli/throws';
 import { CACHE_FILES, READ_WRITE_OWNER } from '~const';
 import { clearCache, decode, save } from '~process/cache';
 import { forEach, o } from '~utils';
@@ -92,11 +92,9 @@ export async function createCaches (hash?: string) {
     const path = join($.dirs.cache, file);
 
     if (await pathExists(path)) {
-      if (file === 'paths') {
-        await save(path, new Map())();
-      } else {
-        await save(path, {})();
-      }
+
+      await save(path, {})();
+
     }
 
   }
@@ -124,7 +122,7 @@ export async function getCaches () {
       if ($.project.credentials !== null) {
         caches({ create: true });
       } else {
-        throws.unknown();
+        unknownProject();
         return;
       }
     } else {
@@ -142,21 +140,15 @@ export async function getCaches () {
     $.cache.uri[file] = join($.dirs.cache, file);
 
     if (await pathExists($.cache.uri[file])) {
-
       q.cache.add(async () => {
-
         $.cache[file] = await decode($.cache.uri[file]);
-
       });
-
     } else {
-
-      $.cache[file] = file === 'paths' ? new Map() : {};
-
+      $.cache[file] = {};
       q.cache.add(save($.cache.uri[file], $.cache[file]));
-
     }
-  };
+
+  }
 
   if ($.mode.prune) {
     q.cache.onIdle().then(() => clearCache());
