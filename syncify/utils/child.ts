cdb1effb24b3ee5b93519889c33cb65b52c2dfc8
Nikolas Savvidis <nicos@gmx.com>
2025-04-26 16:26:09 +0200

    child

--- syncify/utils/child.ts
+++ syncify/utils/child.ts
@@ -2,7 +2,7 @@ import { cyan } from '@syncify/ansi';
 
 import { command } from './utils';
 
-import * as throws from '~cli/throws';
+import { throws } from '~cli/throws';
 
 export async function execAsync (cmd: string) {
 
@@ -14,7 +14,7 @@ export async function execAsync (cmd: string) {
 
   } catch (e) {
 
-    throws.throwError(e, [
+    throws(e, [
       `This ${cyan('child_process')} error and likely unrelated to Syncify.`,
       'It is unclear what has caused the issue, but consult the error message',
       'or please submit an issue on github repository.'
