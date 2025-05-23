ae04c26ce0fe851fc4f9a4430b3761cf4091d457
Nikolas Savvidis <nicos@gmx.com>
2025-05-23 08:19:55 +0200

    distribute

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,1 @@
-'use strict';
-
-var syncify_js = require('./syncify.js');
-
-
-
-Object.defineProperty(exports, "defineConfig", {
-  enumerable: true,
-  get: function () { return syncify_js.t; }
-});
-Object.defineProperty(exports, "env", {
-  enumerable: true,
-  get: function () { return syncify_js.r; }
-});
-Object.defineProperty(exports, "syncify", {
-  enumerable: true,
-  get: function () { return syncify_js.syncify; }
-});
+'use strict';var syncify_js=require('./syncify.js');Object.defineProperty(exports,"defineConfig",{enumerable:true,get:function(){return syncify_js.X}});Object.defineProperty(exports,"env",{enumerable:true,get:function(){return syncify_js.W}});Object.defineProperty(exports,"syncify",{enumerable:true,get:function(){return syncify_js.Y}});
