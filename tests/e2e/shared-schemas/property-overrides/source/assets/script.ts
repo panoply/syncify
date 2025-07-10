7c5ffebd76aff730816fb673529d772c05766ca5
Kim Skinner <git@wolfgrey.dev>
2025-07-10 16:06:19 +0200

    Add e2e test for property overrides in shared schemas

---
+++ tests/e2e/shared-schemas/property-overrides/source/assets/script.ts
@@ -0,0 +1,17 @@
+export function index (param: string) {
+
+  console.log(param);
+
+}
+
+export function collection (param: string) {
+
+  console.log(param);
+
+}
+
+export function product (param: string) {
+
+  console.log(param);
+
+}
